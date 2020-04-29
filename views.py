2866b7f99cce007a996b19f75224bbc85607f3c8
KiranGhumare <51757037+KiranGhumare@users.noreply.github.com>
2020-04-30 04:30:07 +0530

    Add files via upload

--- views.py
+++ views.py
@@ -1,33 +1,94 @@
-from django.views.generic import ListView
 from django.shortcuts import render
-from django.contrib import messages
-from .models import Customer
+from django.http import HttpResponse
+from .models import laundryshop, garment , Contact, Orders, OrderUpdate
+from math import ceil
+import json
 
-def home(request):
-    return render(request, 'customer/home.html')
-def register(request):
-    if request.method == "POST":
-        FirstName = request.POST.get('FirstName', '')
-        LastName = request.POST.get('LastName', '')
-        EmailId = request.POST.get('EmailId', '')
-        PhoneNo = request.POST.get('PhoneNo', '')
-        address = request.POST.get('address', '')
-        Password = request.POST.get('Password','')
+def index(request):
+    # products = Product.objects.all()
+    # print(products)
+    # n = len(products)
+    # nSlides = n//4 + ceil((n/4)-(n//4))
 
-        customer = Customer(FirstName=FirstName, LastName=LastName,address=address, EmailId=EmailId, PhoneNo=PhoneNo, Password=Password )
-        customer.save()
-        a = Customer.FirstName
-        messages.success(request, f'Account created for {a}!')
-    return render(request, 'customer/register.html')
+    allProds = []
+    catprods = laundryshop.objects.values('area', 'id')
+    cats = {item['area'] for item in catprods}
+    for cat in cats:
+        prod = laundryshop.objects.filter(area=cat)
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        allProds.append([prod, range(1, nSlides), nSlides])
 
-def login(request):
+    # params = {'no_of_slides':nSlides, 'range': range(1,nSlides),'product': products}
+    # allProds = [[products, range(1, nSlides), nSlides],
+    #             [products, range(1, nSlides), nSlides]]
+    params = {'allProds': allProds}# products = Product.objects.all()
+    # print(products)
+    # n = len(products)
+    # nSlides = n//4 + ceil((n/4)-(n//4))
 
-    if request.method == 'POST':
-        FirstName=request.POST.get('FirstName', '')
-        Password=request.POST.get('Password', '')
-        customer = Customer.objects.filter(FirstName=FirstName, Password=Password)
-        if customer is not None:
-            return render(request, 'customer-home')
-        else:
-            return render(request, 'customer/login.html')
-    return render(request, 'customer/login.html')
+
+
+    return render(request, 'shop/index.html', params)
+
+
+def about(request):
+    return render(request, 'shop/about.html')
+
+def contact(request):
+    if request.method=="POST":
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        phone = request.POST.get('phone', '')
+        desc = request.POST.get('desc', '')
+        contact = Contact(name=name, email=email, phone=phone, desc=desc)
+        contact.save()
+    return render(request, 'shop/contact.html')
+
+def tracker(request):
+    if request.method=="POST":
+        orderId = request.POST.get('orderId', '')
+        email = request.POST.get('email', '')
+        try:
+            order = Orders.objects.filter(order_id=orderId, email=email)
+            if len(order)>0:
+                update = OrderUpdate.objects.filter(order_id=orderId)
+                updates = []
+                for item in update:
+                    updates.append({'text': item.update_desc, 'time': item.timestamp})
+                    response = json.dumps({"status":"success", "updates": updates, "itemsJson": order[0].items_json}, default=str)
+                return HttpResponse(response)
+            else:
+                return HttpResponse('{"status":"noitem"}')
+        except Exception as e:
+            return HttpResponse('{"status":"error"}')
+
+    return render(request, 'shop/tracker.html')
+def search(request):
+    return render(request, 'shop/search.html')
+
+def pricescheme(request):
+    items = garment.objects.all()
+    print(items)
+
+    return render(request, 'shop/pricescheme.html', {'items': items})
+
+def checkout(request):
+    if request.method=="POST":
+        items_json = request.POST.get('itemsJson', '')
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        address = request.POST.get('address1', '') + " " + request.POST.get('address2', '')
+        city = request.POST.get('city', '')
+        state = request.POST.get('state', '')
+        zip_code = request.POST.get('zip_code', '')
+        phone = request.POST.get('phone', '')
+        order = Orders(items_json=items_json, name=name, email=email, address=address, city=city,
+                       state=state, zip_code=zip_code, phone=phone)
+        order.save()
+        update = OrderUpdate(order_id=order.order_id, update_desc="The order has been placed")
+        update.save()
+        thank = True
+        id = order.order_id
+        return render(request, 'shop/checkout.html', {'thank': thank, 'id': id})
+    return render(request, 'shop/checkout.html')
